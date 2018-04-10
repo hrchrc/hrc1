7d62b0f44ad40cf54f1b55f8f468891dff720ba1
hrc <435995616@qq.com>
2018-04-10 11:06:36 +0800

    提交1

--- Shangchuanceshi/src/cn/jbit/out/AS.java
+++ Shangchuanceshi/src/cn/jbit/out/AS.java
@@ -4,7 +4,7 @@ public class AS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("这是测试");
+		System.out.println("这是测试22");
 	}
 
 }
--- Shangchuanceshi/src/cn/jbit/out/sss.java
+++ Shangchuanceshi/src/cn/jbit/out/sss.java
@@ -4,7 +4,7 @@ public class sss {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("qwert");
+		System.out.println("qwert1");
 	}
 
 }
